c9a019719777c1fd112e264baad4bda741eec5e3
bcova <brandoncova@gmail.com>
2022-06-22 18:53:42 -0500

    getting closer

---
+++ app.js
@@ -0,0 +1,35 @@
+const board = document.getElementById("game")
+
+const input1 = document.getElementById("player1")
+const input2 = document.getElementById("player2")
+const players = document.getElementById("players")
+const name1 = document.getElementById("p1")
+const name2=document.getElementById("p2")
+
+input1.addEventListener('keypress',(Event) => {
+//   console.log(Event)
+ if (Event.key === 'Enter' && input1.value.length > 0 ) {
+    input1.style.display = 'none' 
+    players.style.display = 'flex' 
+    name1.innerHTML = 'P1: '+ input1.value
+ }
+
+})
+
+input2.addEventListener('keypress',(Event) => {
+    // console.log(Event)
+   if (Event.key === 'Enter'&& input2.value.length > 0 ) {
+      input2.style.display = 'none'
+      players.style.display = 'flex'
+      name2.innerHTML = 'P2: '+  input2.value
+   }
+ 
+  })
+
+board.addEventListener('click',spot)
+  
+function spot () {
+    board.getElementById
+}
+
+ 
